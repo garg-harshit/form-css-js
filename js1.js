3dfde9392d9234bdc41b5277d712061390731b7e
Harshit <happygarg7253@gmail.comgit config --global user.email happygarg7253@gmail.com>
2022-08-31 13:15:45 +0530

    initial commit

---
+++ js1.js
@@ -0,0 +1,12 @@
+console.log("Hello every one");
+
+let rollno= "1"
+let rollno1="3"
+let rollno2= "6"
+
+console.log("rollno 1 is Happy");
+console.log("rollno 3 is Shivani");
+console.log("rollno 6 is Shivam");
+alert ("thisis a messag")
+let a =alert("this is a message")  
+console.log(a)
---
+++ js2.js
@@ -0,0 +1,11 @@
+console.log("this is tutorial 56");
+
+let Subject="JS"
+let Subject1="HTML"
+let Subject2="CSS"
+let SRno="1"
+let SRno1="3"
+let SRno2="4"
+console.log("Subject 1 is JS");
+console.log("Subject 2 is HTML");
+console.log("Subject 3 is CSS");
